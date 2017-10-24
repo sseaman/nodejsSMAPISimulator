0f4a2cb527c5447f4c2c641d82b63dbb84fbff0a
Seaman <ssseaman@amazon.com>
2017-10-24 10:10:47 -0700

    rest of files

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "parserOptions" : {
+        "ecmaVersion" : 6,
+        "sourceType" : "module",
+        "ecmaFeatures" : {
+            "impliedStrict" : true
+        }
+    },
+    "env": {
+        "node": true
+    },
+    "rules":{
+        "eqeqeq" : 2,
+        "no-console" : 0,
+        "no-alert" : 2,
+        "no-eq-null" : 2,
+        "no-implied-eval" : 2,
+        "semi" : 2, 
+        "valid-jsdoc" : [ 2, { "requireReturn": false } ]
+    },
+    "globals" : {
+        "Promise" : true,
+        "describe" : true,
+        "it" : true,
+        "before" : true
+    }
+};
